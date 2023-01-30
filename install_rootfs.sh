59b0ee9f7077d4137953d32b5bed79d7dbf7ab6b
zhizhou tian <zhizhou.tian@gmail.com>
2023-01-30 20:52:04 +0800

    enable nvidia-smi
    Signed-off-by: zhizhou tian <zhizhou.tian@gmail.com>

--- install_rootfs.sh
+++ install_rootfs.sh
@@ -5,22 +5,22 @@ set -x
 PWD=$(pwd)
 ROOTFS_DIR="$(realpath kata-containers/tools/osbuilder/rootfs-builder/rootfs)"
 
-#module_dir=${ROOTFS_DIR}/lib/modules/519.2-nvidia-gpu/
-#mkdir -p ${module_dir}
-#
-#sudo install -o root -g root -m 0440 "${PWD}/scripts/nvidia.ko" ${module_dir}
-#sudo install -o root -g root -m 0440 "${PWD}/scripts/nvidia-uvm.ko" ${module_dir}
-#
-## TODO
-#tools="lspci lsmod modprobe modinfo insmod rmmod nvidia-smi"
-#for tool in ${tools}; do
-#	sudo install -o root -g root -m 0740 "$(which ${tool})" "${ROOTFS_DIR}/usr/bin";
-#done
-#
-#sos="libcrypto.so.1.1 libpci.so.3 libkmod.so.2 libcuda.so libnvidia-ml.so"
-#for so in ${sos}; do
-#	sudo install -o root -g root -m 0740 "/lib64/${so}" "${ROOTFS_DIR}/lib64";
-#done
+module_dir=${ROOTFS_DIR}/lib/modules/519.2-nvidia-gpu/
+mkdir -p ${module_dir}
+
+sudo install -o root -g root -m 0440 "${PWD}/scripts/nvidia.ko" ${module_dir}
+sudo install -o root -g root -m 0440 "${PWD}/scripts/nvidia-uvm.ko" ${module_dir}
+
+# TODO
+tools="lspci lsmod modprobe modinfo insmod rmmod nvidia-smi"
+for tool in ${tools}; do
+	sudo install -o root -g root -m 0740 ${PWD}/scripts/${tool} "${ROOTFS_DIR}/usr/bin/";
+done
+
+sos="libcrypto.so.1.1 libpci.so.3 libkmod.so.2 libcuda.so libnvidia-ml.so libnvidia-ml.so.1 libssl.so.1.1 libnuma.so.1"
+for so in ${sos}; do
+	sudo install -o root -g root -m 0740 ${PWD}/scripts/${so} "${ROOTFS_DIR}/lib64";
+done
 
 sudo install -o root -g root -m 0550 -t "${ROOTFS_DIR}/usr/bin" "${ROOTFS_DIR}/../../../../src/agent/target/x86_64-unknown-linux-musl/release/kata-agent"
 sudo install -o root -g root -m 0440 "${ROOTFS_DIR}/../../../../src/agent/kata-agent.service" "${ROOTFS_DIR}/usr/lib/systemd/system/"
